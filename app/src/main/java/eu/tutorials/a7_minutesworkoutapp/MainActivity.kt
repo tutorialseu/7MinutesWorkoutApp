68b1e51acdfe11af39c6bf27ec84e38845f784e9
kulloveth <kulloveth@gmail.com>
2021-09-13 16:59:46 +0100

    add viewbinding and remove action bar

--- app/src/main/java/eu/tutorials/a7_minutesworkoutapp/MainActivity.kt
+++ app/src/main/java/eu/tutorials/a7_minutesworkoutapp/MainActivity.kt
@@ -4,19 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import eu.tutorials.a7_minutesworkoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //  The binding is name just like the name of the layout with Binding attached
+    //Todo 1:We create a variable for it and assign to null
+    private var binding:ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //Todo 2: We inflate the late file by calling inflate on the Binding name
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //Todo 3: Then replace the setContentView parameter with binding?.root
+        setContentView(binding?.root)
 
-        val fLStartButton:FrameLayout = findViewById(R.id.flStart)
-        fLStartButton.setOnClickListener {
+        binding?.flStart?.setOnClickListener {
             Toast.makeText(
                 this@MainActivity,
                 "Here we will start the exercise.",
                 Toast.LENGTH_SHORT
             ).show()
         }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //Todo 4: TO avoid memory leak we unassign the binding once the activity is destroyed
+        binding = null
     }
 }
