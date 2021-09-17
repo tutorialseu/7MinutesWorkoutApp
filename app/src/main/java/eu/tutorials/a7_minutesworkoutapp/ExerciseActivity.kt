b0c792321218a0231f022b6103e38acaa19048f3
kulloveth <kulloveth@gmail.com>
2021-09-17 09:40:51 +0100

    complete displaying the seconds timer

--- app/src/main/java/eu/tutorials/a7_minutesworkoutapp/ExerciseActivity.kt
+++ app/src/main/java/eu/tutorials/a7_minutesworkoutapp/ExerciseActivity.kt
@@ -2,18 +2,29 @@ package eu.tutorials.a7_minutesworkoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import eu.tutorials.a7_minutesworkoutapp.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
-    //Todo 1 creat a binding variable
+
+    //TODO(Step 1 - Adding a variables for the 10 seconds REST timer.)
+    //START
+    private var restTimer: CountDownTimer? =
+        null // Variable for Rest Timer and later on we will initialize it.
+    private var restProgress =
+        0 // Variable for timer progress. As initial value the rest progress is set to 0. As we are about to start.
+    //END
+
+    // create a binding variable
     private var binding:ActivityExerciseBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//Todo 2 inflate the layout
+//inflate the layout
         binding = ActivityExerciseBinding.inflate(layoutInflater)
-//Todo 3 pass in binding?.root in the content view
+// pass in binding?.root in the content view
         setContentView(binding?.root)
-//Todo 4: then set support action bar and get toolBarExcerciser using the binding
+// then set support action bar and get toolBarExcerciser using the binding
 //variable
         setSupportActionBar(binding?.toolbarExercise)
 
@@ -23,5 +34,82 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        setupRestView()
     }
+
+
+    //TODO(Step 3 - Setting up the Get Ready View with 10 seconds of timer.)-->
+    //START
+    /**
+     * Function is used to set the timer for REST.
+     */
+    private fun setupRestView() {
+
+        /**
+         * Here firstly we will check if the timer is running the and it is not null then cancel the running timer and start the new one.
+         * And set the progress to initial which is 0.
+         */
+        if (restTimer != null) {
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+
+        // This function is used to set the progress details.
+        setRestProgressBar()
+    }
+    // END
+
+    //TODO(Step 2 - Setting up the 10 seconds timer for rest view and updating it continuously.)-->
+    //START
+    /**
+     * Function is used to set the progress of timer using the progress
+     */
+    private fun setRestProgressBar() {
+
+        binding?.progressBar?.progress = restProgress // Sets the current progress to the specified value.
+
+        /**
+         * @param millisInFuture The number of millis in the future from the call
+         *   to {#start()} until the countdown is done and {#onFinish()}
+         *   is called.
+         * @param countDownInterval The interval along the way to receive
+         *   {#onTick(long)} callbacks.
+         */
+        // Here we have started a timer of 10 seconds so the 10000 is milliseconds is 10 seconds and the countdown interval is 1 second so it 1000.
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++ // It is increased by 1
+                binding?.progressBar?.progress = 10 - restProgress // Indicates progress bar progress
+                binding?.tvTimer?.text =
+                    (10 - restProgress).toString()  // Current progress is set to text view in terms of seconds.
+            }
+
+            override fun onFinish() {
+                // When the 10 seconds will complete this will be executed.
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "Here now we will start the exercise.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }.start()
+    }
+    //END
+
+
+    //TODO(Step 5 - Destroying the timer when closing the activity or app.)-->
+    //START
+    /**
+     * Here in the Destroy function we will reset the rest timer if it is running.
+     */
+    public override fun onDestroy() {
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        super.onDestroy()
+        binding = null
+    }
+    //END
 }
