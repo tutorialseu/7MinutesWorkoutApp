d89af385bb56399b53efc26b361ba414e02eb245
kulloveth <kulloveth@gmail.com>
2021-09-20 13:42:14 +0100

    create adapter and viewholder

---
+++ app/src/main/java/eu/tutorials/a7_minutesworkoutapp/ExerciseStatusAdapter.kt
@@ -0,0 +1,59 @@
+package eu.tutorials.a7_minutesworkoutapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import eu.tutorials.a7_minutesworkoutapp.databinding.ItemExerciseStatusBinding
+
+// TODO(Step 1 : Creating an adapter class for RecyclerView using the item designed for it and along with Exercise Model class.)
+// START
+class ExerciseStatusAdapter(val items: ArrayList<ExerciseModel>) :
+    RecyclerView.Adapter<ExerciseStatusAdapter.ViewHolder>() {
+
+    /**
+     * Inflates the item view which is designed in xml layout file
+     *
+     * create a new
+     * {@link ViewHolder} and initializes some private fields to be used by RecyclerView.
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemExerciseStatusBinding.inflate(
+            LayoutInflater.from(parent.context),parent,false
+            )
+        )
+    }
+
+    /**
+     * Binds each item in the ArrayList to a view
+     *
+     * Called when RecyclerView needs a new {@link ViewHolder} of the given type to represent
+     * an item.
+     *
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     */
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val model: ExerciseModel = items[position]
+
+        holder.tvItem.text = model.getId().toString()
+    }
+
+    /**
+     * Gets the number of items in the list
+     */
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    /**
+     * A ViewHolder describes an item view and metadata about its place within the RecyclerView.
+     */
+    class ViewHolder(binding: ItemExerciseStatusBinding) : RecyclerView.ViewHolder(binding.root) {
+        // Holds the TextView that will add each item to
+        val tvItem = binding.tvItem
+    }
+}
+// END
