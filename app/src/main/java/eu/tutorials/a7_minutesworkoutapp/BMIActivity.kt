856954b89f8bc3eae49be3f581a36676eac8d412
kulloveth <kulloveth@gmail.com>
2021-09-22 15:44:28 +0100

    add us view elements

--- app/src/main/java/eu/tutorials/a7_minutesworkoutapp/BMIActivity.kt
+++ app/src/main/java/eu/tutorials/a7_minutesworkoutapp/BMIActivity.kt
@@ -2,6 +2,7 @@ package eu.tutorials.a7_minutesworkoutapp
 
 import android.os.Bundle
 import android.view.View
+import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import eu.tutorials.a7_minutesworkoutapp.databinding.ActivityBmiBinding
@@ -9,7 +10,18 @@ import java.math.BigDecimal
 import java.math.RoundingMode
 
 class BMIActivity : AppCompatActivity() {
-    // create activity for the activity
+
+    // TODO(Step 2 : Added variables for METRIC and US UNITS views and a variable for displaying the current selected view..)
+    // START
+    companion object {
+        private const val METRIC_UNITS_VIEW = "METRIC_UNIT_VIEW" // Metric Unit View
+        private const val US_UNITS_VIEW = "US_UNIT_VIEW" // US Unit View
+    }
+
+    private var currentVisibleView: String =
+        METRIC_UNITS_VIEW // A variable to hold a value to make a selected view visible
+    // END
+    // create binding for the activity
     private var binding: ActivityBmiBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +36,24 @@ class BMIActivity : AppCompatActivity() {
         binding?.toolbarBmiActivity?.setNavigationOnClickListener {
             onBackPressed()
         }
+        // TODO(Step 5 : When the activity is launched make METRIC UNITS VIEW visible.)
+        // START
+        makeVisibleMetricUnitsView()
+        // END
+         // TODO(Step 6 : Adding a check change listener to the radio group and according to the radio button.)
+        // START
+        // Radio Group change listener is set to the radio group which is added in XML.
+        //we use _ for the first value because we don't need it
+        binding?.rgUnits?.setOnCheckedChangeListener { _, checkedId: Int ->
 
-
+            // Here if the checkId is METRIC UNITS view then make the view visible else US UNITS view.
+            if (checkedId == R.id.rbMetricUnits) {
+                makeVisibleMetricUnitsView()
+            } else {
+                makeVisibleUsUnitsView()
+            }
+        }
+        // END
         // Button will calculate the input values in Metric Units
         binding?.btnCalculateUnits?.setOnClickListener {
 
@@ -51,7 +79,42 @@ class BMIActivity : AppCompatActivity() {
     }
 
 
+    // TODO(Step 3 : Making a function to make the METRIC UNITS view visible.)
+    // START
+    /**
+     * Function is used to make the METRIC UNITS VIEW visible and hide the US UNITS VIEW.
+     */
+    private fun makeVisibleMetricUnitsView() {
+        currentVisibleView = METRIC_UNITS_VIEW // Current View is updated here.
+        binding?.tilMetricUnitWeight?.visibility = View.VISIBLE // METRIC  Height UNITS VIEW is Visible
+        binding?.tilMetricUnitHeight?.visibility = View.VISIBLE // METRIC  Weight UNITS VIEW is Visible
+        binding?.tilUsMetricUnitWeight?.visibility = View.GONE // make weight view Gone.
+        binding?.tilMetricUsUnitHeightFeet?.visibility = View.GONE // make height feet view Gone.
+        binding?.tilMetricUsUnitHeightInch?.visibility = View.GONE // make height inch view Gone.
 
+        binding?.etMetricUnitHeight?.text!!.clear() // height value is cleared if it is added.
+        binding?.etMetricUnitWeight?.text!!.clear() // weight value is cleared if it is added.
+
+        binding?.llDiplayBMIResult?.visibility = View.INVISIBLE
+    }
+    // END
+
+    // TODO(Step 4 : Making a function to make the US UNITS view visible.)
+    // START
+    private fun makeVisibleUsUnitsView() {
+        currentVisibleView = US_UNITS_VIEW // Current View is updated here.
+        binding?.tilMetricUnitHeight?.visibility = View.INVISIBLE // METRIC  Height UNITS VIEW is InVisible
+        binding?.tilMetricUnitWeight?.visibility = View.INVISIBLE // METRIC  Weight UNITS VIEW is InVisible
+        binding?.tilUsMetricUnitWeight?.visibility = View.VISIBLE // make weight view visible.
+        binding?.tilMetricUsUnitHeightFeet?.visibility = View.VISIBLE // make height feet view visible.
+        binding?.tilMetricUsUnitHeightInch?.visibility = View.VISIBLE // make height inch view visible.
+
+        binding?.etUsMetricUnitWeight?.text!!.clear() // weight value is cleared.
+        binding?.etUsMetricUnitHeightFeet?.text!!.clear() // height feet value is cleared.
+        binding?.etUsMetricUnitHeightInch?.text!!.clear() // height inch is cleared.
+
+       binding?.llDiplayBMIResult?.visibility = View.INVISIBLE
+    }
     /**
      * Function is used to validate the input values for METRIC UNITS.
      */
