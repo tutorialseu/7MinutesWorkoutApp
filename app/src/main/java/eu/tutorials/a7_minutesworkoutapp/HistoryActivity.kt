1fa2af6d6214e70d6915d24256949f0872291680
kulloveth <kulloveth@gmail.com>
2021-10-12 16:59:51 +0100

    update todo

--- app/src/main/java/eu/tutorials/a7_minutesworkoutapp/HistoryActivity.kt
+++ app/src/main/java/eu/tutorials/a7_minutesworkoutapp/HistoryActivity.kt
@@ -4,17 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import eu.tutorials.a7_minutesworkoutapp.databinding.ActivityHistoryBinding
 
+// TODO(Step 6 : Adding the History Screen Activity.)
 class HistoryActivity : AppCompatActivity() {
-    //Todo create a binding for the layout
+    //Todo 7 create a binding for the layout
     private var binding: ActivityHistoryBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//Todo inflate the layout
+//Todo  8 inflate the layout
         binding = ActivityHistoryBinding.inflate(layoutInflater)
-//Todo bind the layout to this activity
+//Todo 9 bind the layout to this activity
         setContentView(binding?.root)
 
-//TODO(Step 5 : Setting up the action bar in the History Screen Activity and
+//TODO(Step 11 : Setting up the action bar in the History Screen Activity and
 // adding a back arrow button and click event for it.)
 // START
         setSupportActionBar(binding?.toolbarHistoryActivity)
@@ -34,7 +35,7 @@ class HistoryActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-//Todo reset the binding to null to avoid memory leak
+//Todo 12 reset the binding to null to avoid memory leak
         binding = null
     }
 }
