e13313c7d7608278c3728102897df41c42e77f72
kulloveth <kulloveth@gmail.com>
2021-10-12 18:18:43 +0100

    update todo

--- app/src/main/java/eu/tutorials/a7_minutesworkoutapp/MainActivity.kt
+++ app/src/main/java/eu/tutorials/a7_minutesworkoutapp/MainActivity.kt
@@ -20,8 +20,6 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        //TODO(Step 6 : Adding a click event to the BMI calculator button and navigating it to the BMI calculator feature.)
-        //START
         binding?.flBMI?.setOnClickListener {
             // Launching the BMI Activity
             val intent = Intent(this, BMIActivity::class.java)
